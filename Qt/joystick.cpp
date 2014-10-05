32d2da4ef093c93c12e7a8529fcfdfc5bb539a17
Vince <vincentzhang1996@gmail.com>
2014-10-05 14:42:29 -0400

    More Joystick stuff

--- Qt/joystick.cpp
+++ Qt/joystick.cpp
@@ -1,2 +1,15 @@
 #include "joystick.h"
+#include <QDebug>
+
+Joystick::Joystick() {
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK);
+
+    stick = SDL_JoystickOpen(0);
+    qDebug() << SDL_JoystickNumAxes(stick);
+}
+
+Joystick::~Joystick() {
+
+}
+
 
--- Qt/joystick.h
+++ Qt/joystick.h
@@ -1,12 +1,18 @@
 #ifndef JOYSTICK_H
 #define JOYSTICK_H
 
+#include <QObject>
 #include "SDL.h"
 
-class Joystick {
+class Joystick : public QObject {
+    Q_OBJECT
 
 public:
     Joystick();
+    ~Joystick();
+
+private:
+    SDL_Joystick* stick;
 
 };
 
