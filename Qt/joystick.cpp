3fd179b6b6fdffcc95e0b64c5a53509d4996175e
Vincent Zhang <vzhang@purdue.edu>
2014-12-06 18:40:51 -0500

    Fixed off by 1 issue with axes, no more float stuff
    Didn't really need float values

--- Qt/joystick.cpp
+++ Qt/joystick.cpp
@@ -1,15 +1,5 @@
 #include "joystick.h"
 
-#define qint16_MAX 32767
-#define qint16_MIN -32768
-#define qint16_MIN_ABS 32768
-#define UINT16_MAX_F 65535.0
-
-float axisRawToFloat(qint16 val) {
-    val += qint16_MIN_ABS;
-    return ((float (val)) / UINT16_MAX_F) - 1.0;
-}
-
 Joystick::Joystick(int joystickId) :
     m_joystickId(joystickId) {
 
@@ -44,14 +34,10 @@ QString* Joystick::init() {
 }
 
 qint16 Joystick::getAxis(int axisId) {
-    if (axisId < m_axes.length() && axisId > 0) {
+    if (axisId < m_axes.length() && axisId >= 0) {
         return m_axes.at(axisId);
     }
     return 0;
-}
-
-float Joystick::getAxisF(int axisId) {
-    return axisRawToFloat(getAxis(axisId));
 }
 
 bool Joystick::getButtonState(int buttonId) {
