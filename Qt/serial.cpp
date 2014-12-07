6d7eb948af5b1494e28d523e0a4bd0766194c8cb
Matt Molo <molo.matt@gmail.com>
2014-12-06 19:16:14 -0500

    Serial implementation with crc-8

--- Qt/serial.cpp
+++ Qt/serial.cpp
@@ -11,10 +11,18 @@ void serial::initSerial(QString device) {
 }
 
 
-bool serial::send(char bytes[], size_t size) {
+bool serial::send(quint8 bytes[], quint8 size) {
+
+    //print(bytes, size);
+    //qDebug() << "\nNew packet\n";
+    //bytes[size-2] = crc8(bytes, size);
+    //print(bytes, size);
+
+    //do crc8 on checksum byte
+    bytes[size-2] = crc8(bytes, size);
 
     if (serialDevice.open(QIODevice::ReadWrite) && serialDevice.isWritable()) {
-        QByteArray data = QByteArray::fromRawData(bytes, size);
+        QByteArray data = QByteArray::fromRawData((char*) bytes, size);
         serialDevice.write(data);
         return serialDevice.flush();
     }
@@ -23,4 +31,31 @@ bool serial::send(char bytes[], size_t size) {
 }
 
 
+quint8 serial::crc8(quint8 bytes[], quint8 size) {
 
+    quint8 crc = 0;
+
+    for (int i = 0; i < size; ++i) {
+
+        quint8 inbyte = bytes[i];
+
+        for (int i = 8; i; i--) {
+          quint8 mix = (crc ^ inbyte) & 0x01;
+          crc >>= 1;
+          if (mix) crc ^= 0xD5;
+          inbyte >>= 1;
+        }
+
+      }
+
+    return crc;
+}
+
+void serial::print(quint8 bytes[], quint8 size) {
+
+    for (int i = 0; i < size; ++i) {
+        bytes[i] = bytes[i] & 0xFF;
+        qDebug("[%x]: [%d]", bytes[i], bytes[i]);
+    }
+
+}
