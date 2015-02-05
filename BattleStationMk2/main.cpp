00747338df2300a2787112f12ccd70d82ff7f430
Vincent Zhang <vzhang@purdue.edu>
2015-02-05 15:30:56 -0500

    Testing some QML <-> C++, close

--- BattleStationMk2/main.cpp
+++ BattleStationMk2/main.cpp
@@ -1,12 +1,20 @@
 #include <QApplication>
-#include <QQmlApplicationEngine>
+#include <QQmlEngine>
+#include <QQmlComponent>
+#include <QQmlContext>
+#include "qmlbattlestationcontext.h"
+#include "serial.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-
+    QQmlEngine engine;
+    QMLBattleStationContext context;
+    Serial serial;
+    context.SetObject("serial", &serial);
+    engine.rootContext()->setContextProperty(QString("context"), &context);
+    QQmlComponent component(&engine, QUrl(QStringLiteral("qrc:/main.qml")));
+    component.create();
     return app.exec();
 }
--- BattleStationMk2/qmlbattlestationcontext.h
+++ BattleStationMk2/qmlbattlestationcontext.h
@@ -12,21 +12,34 @@
 class QMLBattleStationContext : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QMap<QString, void *> objects READ Objects NOTIFY ObjectsChanged)
 
     /**
      * This is scary. Register things to this, fetch them from QML by name.
      * I don't know if this will work.
      */
-    QMap<QString, void*> objects_;
+    QMap<QString, void *> m_objects;
 
 
 public:
     explicit QMLBattleStationContext(QObject *parent = 0);
     ~QMLBattleStationContext();
 
-signals:
+    void SetObject(QString name, void * object) {
+        m_objects.insert(name, &object);
+    }
 
+    /**
+     * Objects property getter
+     */
+    QMap<QString, void*> Objects() const {
+        return m_objects;
+    }
+
+signals:
+    void ObjectsChanged();
 public slots:
+
 };
 
 #endif // QMLBATTLESTATIONCONTEXT_H
