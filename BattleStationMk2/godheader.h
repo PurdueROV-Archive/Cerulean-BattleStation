4df5b6759787334054c64bf065c9776b07d391f5
Vincent Zhang <vzhang@purdue.edu>
2015-02-14 16:56:07 -0500

    Added interpolator

--- BattleStationMk2/godheader.h
+++ BattleStationMk2/godheader.h
@@ -7,6 +7,29 @@
 
 #include <QObject>
 
+struct Interpolator {
+    float current;
+    float target;
+    float max_delta_per_tick;
+
+    float lerp(float new_target) {
+        target = new_target;
+        if (current == target) {
+            return current;
+        }
+        float delta = target - current;
+        if (abs(delta) > maxDelta) {
+            delta = (delta < 0 ? -1.0 : 1.0) * max_delta_per_tick;
+        }
+        current += delta;
+        return current;
+    }
+
+    float lerp() {
+        return lerp(target);
+    }
+};
+
 
 
 #endif // GODHEADER
