b87414b11cae41e4a812b58fa135b34bf88b7a5c
Vincent Zhang <vzhang@purdue.edu>
2015-04-11 18:38:26 -0400

    Tying things together using signals and slots

--- BattleStationMk2/main.cpp
+++ BattleStationMk2/main.cpp
@@ -16,8 +16,6 @@ int main(int argc, char *argv[])
     QQmlComponent component(&engine, QUrl(QStringLiteral("qrc:/main.qml")));
     component.create();
     //  Init/create subsystems
-
-    //  SDL
     if (SDL_Init(SDL_INIT_JOYSTICK)) {
         QString* err = new QString(SDL_GetError());
         qWarning() << LOG_ERROR << "SDL init error:" << err;
@@ -25,10 +23,20 @@ int main(int argc, char *argv[])
     } else {
         qDebug() << LOG_OK << "SDL init";
     }
-
-    //  Serial
     Serial* serial = new Serial();
+    InputHandler* inputHandler = new InputHandler();
 
+    //  Connect Serial and BSC
+    QObject::connect(context, SIGNAL(SelectedSerialDeviceChanged(QString)),
+                     serial, SLOT(SetActiveSerialDevice(QString)));
+    //  Connect Input and BSC
+    QObject::connect(context, SIGNAL(SelectedJoystickAChanged(QString)),
+                     inputHandler, SLOT(SetMainJoystick(QString*)));
+    //  Connect Input to Serial
+    QObject::connect(inputHandler, SIGNAL(MotorValuesChanged(quint8[])),
+                     serial, SLOT(SetMotorValues(quint8[])));
+    QObject::connect(inputHandler, SIGNAL(PostToolUpdate(quint16,quint16)),
+                     serial, SLOT(EnqueueToolEvent(quint16,quint16)));
 
 
     return app.exec();
